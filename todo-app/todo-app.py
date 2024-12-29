ca669dcdd1fd3d8f238ba8329ccbafbdd59625a0
SamahithM <gowdasamahithm@gmail.com>
2024-12-29 15:12:54 +0000

    add done

--- todo-app/todo-app.py
+++ todo-app/todo-app.py
@@ -4,11 +4,13 @@ def todo():
         userInp = input("Enter add , show, edit, complete: ")
         if userInp.startswith('add'):
             userInp = userInp.strip("add")
-            with open('todo-app.txt', 'r') as file:
+            with open('todo.txt', 'r') as file:
                 todos = file.readlines()
             todos.append(userInp)
-            with open('todo-app.txt', 'w') as file:
+            with open('todo.txt', 'w') as file:
                 for todo in todos:
                     file.write(todo)
         else:
              exit()
+
+todo()
