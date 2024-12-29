efdb7ca6ceb90b866835ba7d8bfd83154fb341bc
SamahithM <gowdasamahithm@gmail.com>
2024-12-29 15:11:22 +0000

    notes

--- todo-app/todo-app.py
+++ todo-app/todo-app.py
@@ -1,9 +1,14 @@
 todos = []
-
-while True:
-    userInp = input("Enter add , show, edit, complete: ")
-    if userInp.startswith('add'):
-        userInp = userInp.strip("add")
-        with open('/home/smg/Documents/PYTHON-PROJECTS/todo-app/todo-app.txt', 'r') as file:
-            return file.readlines()
-        todos = file.readlines()
+def todo():
+    while True:
+        userInp = input("Enter add , show, edit, complete: ")
+        if userInp.startswith('add'):
+            userInp = userInp.strip("add")
+            with open('todo-app.txt', 'r') as file:
+                todos = file.readlines()
+            todos.append(userInp)
+            with open('todo-app.txt', 'w') as file:
+                for todo in todos:
+                    file.write(todo)
+        else:
+             exit()
